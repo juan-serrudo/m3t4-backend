60826d06b5ce1d39c9bccd9abc66397075426d7e
Juan Victor Serrudo Chavez <juan.serrudo@ucb.edu.bo>
2025-05-22 01:30:36 -0400

    Se configuro el archivo app.service.js

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { DateTime } from 'luxon';
+import { ResponseDTO } from './dto/response.dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly configService: ConfigService) {}
+
+  getPing(): ResponseDTO {
+    const packageJson = this.configService.get<any>('packageJson');
+
+    return {
+      error: false,
+      message: packageJson?.description,
+      response: {
+        data: {
+          author: packageJson.author,
+          dateTimeServer: DateTime.now().toJSDate(),
+          nameApp: packageJson?.name,
+          version: packageJson?.version,
+        },
+      },
+      status: 200,
+    };
   }
 }
---
+++ src/dto/response.dto.ts
@@ -0,0 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Expose } from 'class-transformer';
+import { IsBoolean, IsNumber, IsString } from 'class-validator';
+
+export class ResponseDTO {
+  @Expose()
+  @IsBoolean()
+  @ApiProperty({ description: 'Columna a ordernar', required: true })
+  error: boolean;
+
+  @Expose()
+  @IsString()
+  @ApiProperty({ description: 'Mensaje general del endpoint', required: false })
+  message: string;
+
+  @Expose()
+  @ApiProperty({ description: 'Contenido del mensaje', required: false })
+  response: any;
+
+  @Expose()
+  @IsNumber()
+  @ApiProperty({ description: 'El código del error', required: false })
+  status: number;
+}
