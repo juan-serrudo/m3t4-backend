db0e77d678b4a808abbd9ed0d7ea886c3df19d1d
Juan Victor Serrudo Chavez <juan.serrudo@ucb.edu.bo>
2025-05-25 17:53:37 -0400

    Avanzando en invetory

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -2,13 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { InventoryController } from './inventory.controller';
 import { InventoryService } from './inventory.service';
+import { inventorysProviders } from './infrastructure/persistence/inventory.providers';
 
 @Module({
   controllers: [
     InventoryController
   ],
   providers: [ 
-    InventoryService   
+    InventoryService,
+    ...inventorysProviders
+  ],
+  exports: [
+    ...inventorysProviders,
   ],
 })
 export class InventoryModule {}
--- src/inventory/inventory.service.ts
+++ src/inventory/inventory.service.ts
@@ -1,7 +1,41 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ResponseDTO } from 'src/shared/dto/response.dto';
+
+import { Inventorys } from './domain/entities/inventorys.entity';
 
 @Injectable()
 export class InventoryService {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(
+    @Inject('INVENTORYS_REPOSITORY')
+    private inventorysRepository: Repository<Inventorys>,
+  ) {}
+
+  async findAll(): Promise<ResponseDTO> {
+    // Inicio
+    let response: ResponseDTO = {
+      error: true,
+      message: 'Error en el sericio',
+      response: [],
+      status: 422
+    }
+
+    // Proceso
+    try {
+      response.error = false;
+      response.message = 'La consulta a la base de datos se realizó correctamente.';
+      response.response = await this.inventorysRepository.find();
+      response.status = 200;
+
+    } catch (error) {
+      response.error = true;
+      response.message = 'Error en la consulta.';
+      response.response = error;
+      response.status = 502;
+    }
+
+    //Fin
+    return response;
+  }
+
 }
